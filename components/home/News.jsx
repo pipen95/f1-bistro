bdcad3bc84ad784606259bdfb1c01e394806d8c3
Pierre Penel <penel.pierre@gmail.com>
2022-03-21 21:37:42 -0400

    News: added Twitter timeline

--- components/home/News.jsx
+++ components/home/News.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 function News() {
   return (
     <div className="News">
-      <a
-        className="twitter-timeline"
-        data-height="600"
-        href="https://twitter.com/F1?ref_src=twsrc%5Etfw"
-      >
-        Tweets by F1
-      </a>
-      ;
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="f1"
+        options={{ height: 650 }}
+      />
     </div>
   );
 }
