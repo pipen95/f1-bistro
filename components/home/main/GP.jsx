297b8517808cd11ca865023f1516a8f616be57bf
Pierre Penel <penel.pierre@gmail.com>
2022-04-06 08:52:29 -0400

    Index: Add ServerSide Props Stats

--- components/home/main/GP.jsx
+++ components/home/main/GP.jsx
@@ -1,38 +1,19 @@
 import Standings from './standings/Standings';
-import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+import Context from './../../../components/Context';
 
 function GP() {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
+  const { raceResults } = useContext(Context);
+  console.log(raceResults);
 
-  useEffect(() => {
-    setLoading(true);
-    fetch('http://ergast.com/api/f1/current/last/results.json')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="GP">
-        <div className="loader-container">
-          <div className="loader"></div>
-        </div>
-      </div>
-    );
-  }
-
-  if (!data) return <p>No profile data</p>;
+  if (!raceResults) return <p>No profile data</p>;
 
   return (
     <div className="GP">
       <div className="Title-GP text-center">
-        <h2>{data.MRData.RaceTable.Races[0].raceName} results</h2>
+        <h2>{raceResults.MRData.RaceTable.Races[0].raceName} results</h2>
       </div>
-      <Standings data={data} />
+      <Standings data={raceResults} />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
 import Main from './../components/home/main/Main';
 import Side from './../components/home/side/Side';
 import Topics from './../components/home/Topics';
+import Context from '../components/Context';
 
-const Home = () => {
+const Home = ({ raceResults }) => {
   return (
     <div className="home">
-      <Main />
-      <Side />
-      <Topics />
+      <Context.Provider value={{ raceResults }}>
+        <Main />
+        <Side />
+        <Topics />
+      </Context.Provider>
     </div>
   );
 };
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://ergast.com/api/f1/current/last/results.json`);
+  const raceResults = await res.json();
+
+  // Pass data to the page via props
+  return { props: { raceResults } };
+}
+
 export default Home;
