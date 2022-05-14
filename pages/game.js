047d60b30c69efe3153ec7e57cb5e99a5c35fb7b
Pierre Penel <penel.pierre@gmail.com>
2022-05-14 10:24:37 -0400

    Game: No stacking

--- pages/game.js
+++ pages/game.js
@@ -13,33 +13,53 @@ const gameReducer = (state, action) => {
       const driverIdx = drivers.findIndex(
         (el) => el.id === `${action.payload.id}`
       );
-      return {
-        ...state,
-        drivers: [
-          ...drivers.slice(0, driverIdx), // before the one we are updating
-          {
-            id: action.payload.id,
-            location: action.payload.location,
-          },
-          ...drivers.slice(driverIdx + 1), // after the one we are updating
-        ],
-      };
+
+      const driverLocationTaken = drivers.filter(
+        (el) =>
+          el.location !== 'side' && el.location === `${action.payload.location}`
+      );
+
+      if (driverLocationTaken.length == 0) {
+        return {
+          ...state,
+          drivers: [
+            ...drivers.slice(0, driverIdx), // before the one we are updating
+            {
+              ...drivers[driverIdx],
+              location: action.payload.location,
+            },
+            ...drivers.slice(driverIdx + 1), // after the one we are updating
+          ],
+        };
+      } else {
+        return state;
+      }
+
     case actionsTypes.BONUS_SET:
       const bonusIdx = bonus.findIndex(
         (el) => el.id === `${action.payload.id}`
       );
-      return {
-        ...state,
-        bonus: [
-          ...bonus.slice(0, bonusIdx), // before the one we are updating
-          {
-            id: action.payload.id,
-            location: action.payload.location,
-            text: action.payload.text,
-          },
-          ...bonus.slice(bonusIdx + 1), // after the one we are updating
-        ],
-      };
+
+      const bonusLocationTaken = bonus.filter(
+        (el) =>
+          el.location !== 'side' && el.location === `${action.payload.location}`
+      );
+
+      if (bonusLocationTaken.length == 0) {
+        return {
+          ...state,
+          bonus: [
+            ...bonus.slice(0, bonusIdx), // before the one we are updating
+            {
+              ...bonus[bonusIdx],
+              location: action.payload.location,
+            },
+            ...bonus.slice(bonusIdx + 1), // after the one we are updating
+          ],
+        };
+      } else {
+        return state;
+      }
 
     case actionsTypes.RESET:
       return {
