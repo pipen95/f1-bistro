acf47ca6a0379139adf9a093bc45e5f8e8ab74e3
Pierre Penel <penel.pierre@gmail.com>
2022-11-12 11:20:43 -0500

    Modal OK

--- pages/game/Side.jsx
+++ pages/game/Side.jsx
@@ -1,11 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { gameContext } from './context/Context';
 import Driver from './Driver';
 import Bonus from './Bonus';
 import actionsTypes from '././types/actions';
+import Modal from 'components/ui/Modal';
 
 const Side = () => {
   const { state, dispatch, handleSave } = useContext(gameContext);
+  const [isOpen, setIsOpen] = useState(false);
+
   const reset = () => {
     dispatch({
       type: actionsTypes.RESET,
@@ -25,11 +28,51 @@ const Side = () => {
   });
 
   return (
-    <div className="Selection">
-      <div className="Rules">
-        <h3 className="mb-2">Make your predections for the X Grand Prix</h3>
+    <>
+      <div className="Selection">
+        <div className="Rules">
+          <h3 className="mb-2">Make your predections for the X Grand Prix</h3>
+
+          <p>
+            Check out the rules here:
+            <a
+              className="btn btn--blue no-upper nowrap ml-1"
+              onClick={() => setIsOpen(true)}
+            >
+              View rules
+            </a>
+          </p>
+        </div>
+        <div div className="Choices">
+          <div className="Drivers">
+            <h4>Drivers</h4>
+            <div className="Drivers__box">{driversItems}</div>
+          </div>
+          <div className="Bonus">
+            <h4>Bonus</h4>
+            <div className="Bonus__box">{bonusItems}</div>
+          </div>
+          <div className="Actions">
+            <h4>Actions</h4>
+            <div className="Actions__box">
+              <div className="Actions__item">
+                <a onClick={reset} className="btn btn--blue">
+                  Reset
+                </a>
+              </div>
+              <div className="Actions__item">
+                <a onClick={handleSave} className="btn btn--blue">
+                  Save
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         <h4>Rules:</h4>
-        <ol>
+        <ol className="mb-2">
           <li>
             Drag and Drop the <strong>Drivers</strong> from the list below into
             the Grid slots on the left.
@@ -39,41 +82,30 @@ const Side = () => {
             accomplishemnts. Ex: fastest lap, driver of the day, best
             qualification etc.
           </li>
-          <li>You can change your vote up 15min to before the&nbsp;GP.</li>
-          <li>
-            See how points are awarded
-            <a href="#" className="btn-text nowrap">
-              Click here
-            </a>
-          </li>
+
+          <li>You can change your vote up 15 min to before the&nbsp;GP.</li>
         </ol>
-      </div>
-      <div div className="Choices">
-        <div className="Drivers">
-          <h4>Drivers</h4>
-          <div className="Drivers__box">{driversItems}</div>
-        </div>
-        <div className="Bonus">
-          <h4>Bonus</h4>
-          <div className="Bonus__box">{bonusItems}</div>
-        </div>
-        <div className="Actions">
-          <h4>Actions</h4>
-          <div className="Actions__box">
-            <div className="Actions__item">
-              <a onClick={reset} className="btn btn--blue">
-                Reset
-              </a>
-            </div>
-            <div className="Actions__item">
-              <a onClick={handleSave} className="btn btn--blue">
-                Save
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+        <h4>Points:</h4>
+        <table style={{ textAlign: 'left' }}>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
+      </Modal>
+    </>
   );
 };
 
