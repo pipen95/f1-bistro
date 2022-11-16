768c916afaf58050ada8382bbcc739bf4111e81a
Pierre Penel <penel.pierre@gmail.com>
2022-11-16 07:17:21 -0500

    Card restyle OK

--- pages/home/main/standings/Podium.jsx
+++ pages/home/main/standings/Podium.jsx
@@ -23,7 +23,9 @@ function Podium({ data }) {
               <h4
                 className={`card__heading card__heading--${data[1].Constructor.constructorId.toLowerCase()}`}
               >
-                <span className="card__heading-span card__heading-span--2">
+                <span
+                  className={`card__heading-span card__heading-span--${data[1].Constructor.constructorId.toLowerCase()}`}
+                >
                   2<small>nd</small>
                 </span>
               </h4>
@@ -46,24 +48,27 @@ function Podium({ data }) {
               />
             </div>
             <div
-              className="card__side  card__side--front  card__side--front-1"
+              className={`card__side  card__side--front card__side--front--${data[0].Constructor.constructorId.toLowerCase()}`}
               id="side-winner"
             >
               <div
-                className={`card__picture card__picture--${data[1].Constructor.constructorId.toLowerCase()}`}
+                className={`card__picture card__picture--${data[0].Constructor.constructorId.toLowerCase()}`}
                 style={{ height: '12rem' }}
               ></div>
               <h4
-                className="card__heading card__heading--1"
-                id="heading-winner"
+                className={`card__heading card__heading--${data[0].Constructor.constructorId.toLowerCase()}`}
               >
-                <span className="card__heading-span card__heading-span--1">
+                <span
+                  className={`card__heading-span card__heading-span--${data[0].Constructor.constructorId.toLowerCase()}`}
+                  id="heading-winner"
+                >
                   1<small>st</small>
                 </span>
               </h4>
             </div>
+
             <div
-              className="card__side  card__side--back  card__side--back-1"
+              className={`card__side  card__side--back  card__side--back--${data[1].Constructor.constructorId.toLowerCase()}`}
               id="side-winner"
             >
               <p>{`${data[0].Driver.givenName}\u00a0${data[0].Driver.familyName}`}</p>
@@ -76,20 +81,29 @@ function Podium({ data }) {
           <div className="card">
             <div className="driverImg driverImg--3">
               <img
-                alt={`${data[2].Driver.familyName}`}
+                alt={`${data[2].Driver.FamilyName}`}
                 src={`https://res.cloudinary.com/f1-fantasy-tracker/image/upload/f_auto/v1618736029/FullBody/${data[2].Driver.familyName}Full.png`}
               />
             </div>
-
-            <div className="card__side  card__side--front  card__side--front-bis-2">
-              <div className="card__picture card__picture--3"></div>
-              <h4 className="card__heading card__heading--3">
-                <span className="card__heading-span card__heading-span--3">
+            <div
+              className={`card__side  card__side--front card__side--front--${data[2].Constructor.constructorId.toLowerCase()}`}
+            >
+              <div
+                className={`card__picture card__picture--${data[2].Constructor.constructorId.toLowerCase()}`}
+              ></div>
+              <h4
+                className={`card__heading card__heading--${data[2].Constructor.constructorId.toLowerCase()}`}
+              >
+                <span
+                  className={`card__heading-span card__heading-span--${data[2].Constructor.constructorId.toLowerCase()}`}
+                >
                   3<small>rd</small>
                 </span>
               </h4>
             </div>
-            <div className="card__side  card__side--back  card__side--back-3">
+            <div
+              className={`card__side card__side--back card__side--back--${data[2].Constructor.constructorId.toLowerCase()}`}
+            >
               <p>{`${data[2].Driver.givenName}\u00a0${data[2].Driver.familyName}`}</p>
               <p className="card__side--back-points">
                 {data[2].points}&nbsp;pts
